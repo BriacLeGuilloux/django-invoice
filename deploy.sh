3a36a774633b7fd838cbdeaf55efa74458096770
donaldte <donaldtedom0@gmail.com>
2024-09-18 04:15:55 +0100

    add: deploy.sh file

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Pull the latest changes
+cd  /var/www/invoice/
+git pull origin main  # Change 'main' to your branch if needed
+
+# Build and start the Docker containers
+docker-compose up -d
+
+echo "Deployment completed."
