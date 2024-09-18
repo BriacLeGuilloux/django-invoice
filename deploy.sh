c6a591f80fb9a6063bfc0e07476123295b2b2f68
donaldte <donaldtedom0@gmail.com>
2024-09-18 04:31:50 +0100

    fix: chg directory

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Pull the latest changes
-cd  /var/www/invoice/
+cd  /var/www/invoice/django-invoice/
 git pull origin main  # Change 'main' to your branch if needed
 # Build and start the Docker containers
 docker compose up -d
